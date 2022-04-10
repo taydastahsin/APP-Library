45d335e08437649d4087c73754d1ca025a0e64c2
Tahsin Taydaş <56533800+taydastahsin@users.noreply.github.com>
2022-04-10 18:09:55 +0300

    Add files via upload

--- library_control.py
+++ library_control.py
@@ -1,7 +1,10 @@
 from PyQt5.QtWidgets import *
 from library import Ui_MainWindow
+import sqlite3
+from PyQt5.QtSql import *
 
 class library_admin (QMainWindow) :
+
     def __init__(self):
         super().__init__()
         self.ui=Ui_MainWindow()
@@ -14,6 +17,7 @@ class library_admin (QMainWindow) :
         self.book_look_index=4
         self.book_find_index=5
         self.app_pin_trans_index=6
+
 
         self.ui.pushButton.clicked.connect(self.home)
         self.ui.pushButton_2.clicked.connect(self.book_add)
@@ -67,10 +71,22 @@ class library_admin (QMainWindow) :
 
 
     def login(self):
-        a=self.ui.lineEdit.text()
-        b=self.ui.lineEdit_2.text()
+        baglanti = sqlite3.connect("DbLibrary.db")
+        islem = baglanti.cursor()
+        baglanti.commit()
 
-        if a=="admin" and b =="1234" :
+        table = islem.execute(
+            "create table if not exists login(adminName text,adminPin int)")
+        baglanti.commit()
+
+        a=self.ui.lineEdit.text()
+        b=int(self.ui.lineEdit_2.text())
+        # insert yerine update kullan // Giriş id ve şifreyi veritabanına eşitleyip koşullandır.
+        #ekle = "insert into login (adminName,adminPin) values (?,?)"
+        #islem.execute(ekle, (a, b))
+        #baglanti.commit()
+
+        if a == "admin" and b == 1234:
             self.ui.pushButton.setEnabled(True)
             self.ui.pushButton_2.setEnabled(True)
             self.ui.pushButton_7.setEnabled(True)
@@ -85,7 +101,7 @@ class library_admin (QMainWindow) :
             self.ui.lineEdit_2.clear()
 
         else:
-            QMessageBox.critical(self,"Hatalı Giriş","Lütfen id ve şifrenizi doğru giriniz.")
+            QMessageBox.critical(self, "Hatalı Giriş", "Lütfen id ve şifrenizi doğru giriniz.")
 
             self.ui.lineEdit.clear()
             self.ui.lineEdit_2.clear()
@@ -93,17 +109,93 @@ class library_admin (QMainWindow) :
 
 
 
+
     def book_add_save(self):
-        pass
+        baglanti = sqlite3.connect("DbLibrary.db")
+        islem = baglanti.cursor()
+        baglanti.commit()
+
+        table = islem.execute(
+            "create table if not exists bookadd(bookName text,bookNo int,bookWriter text,bookHome text,bookTranstle text,bookPage int)")
+        baglanti.commit()
+
+        a = self.ui.lineEdit_3.text()
+        b = int(self.ui.lineEdit_4.text())
+        c = self.ui.lineEdit_5.text()
+        d = self.ui.lineEdit_6.text()
+        e = self.ui.lineEdit_7.text()
+        f = int (self.ui.lineEdit_8.text())
+
+        try:
+            ekle= "insert into bookadd (bookName,bookNo,bookWriter,bookHome,bookTranstle,bookPage) values (?,?,?,?,?,?)"
+            islem.execute(ekle,(a,b,c,d,e,f))
+            baglanti.commit()
+        except :
+            QMessageBox.warning(self, "Hata Mesajı", "Veri Kaydı Yapılamadı .")
+
+
+
+
+
+
+
+
     def book_get_save(self):
-        pass
+        # Not: Otomatik zaman eklenecek
+        baglanti = sqlite3.connect("DbLibrary.db")
+        islem = baglanti.cursor()
+        baglanti.commit()
+
+        table = islem.execute(
+            "create table if not exists bookget(bookName text,bookNo int,studentName text,studentSurname text,studentNo int,telNo int)")
+        baglanti.commit()
+
+        a = self.ui.lineEdit_9.text()
+        b = int(self.ui.lineEdit_10.text())
+        c = self.ui.lineEdit_11.text()
+        d = self.ui.lineEdit_12.text()
+        e = int(self.ui.lineEdit_13.text())
+        f = int(self.ui.lineEdit_14.text())
+
+
+        try:
+            ekle= "insert into bookget (bookName,bookNo,studentName,studentSurname,studentNo,telNo) values (?,?,?,?,?,?)"
+            islem.execute(ekle,(a,b,c,d,e,f))
+            baglanti.commit()
+        except :
+            QMessageBox.warning(self, "Hata Mesajı", "Veri Kaydı Yapılamadı .")
+
+
+
+
+
+
+
     def book_take_save(self):
+        #Silme işlemi yapılcak
         pass
+
     def book_look_button(self):
-        pass
+        db = sqlite3.connect("DbLibrary.db")
+        cursor = db.cursor()
+
+        command =""" select * from bookget"""
+
+        result =cursor.execute(command)
+
+        self.ui.tableWidget.setRowCount(0)
+
+        for row_number,row_data in enumerate(result):
+            self.ui.tableWidget.insertRow(row_number)
+            for column_number,data in enumerate(row_data):
+                self.ui.tableWidget.setItem(row_number,column_number,QTableWidgetItem(str(data)))
+
     def book_find_button(self):
+        #Bulma yapılcak
         pass
+
     def app_pin_trans_button(self):
+        #Update yapılcak
         pass
 
 
